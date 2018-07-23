4aa6aefc8fd84fb8a34206a981610886c69dc129
Alan <ustargab@gmail.com>
2018-07-23 19:22:06 +0200

    update

--- src/test/java/com/alan/example/springcloudserver/SpringCloudServerApplicationTests.java
+++ src/test/java/com/alan/example/springcloudserver/SpringCloudServerApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringCloudServerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
